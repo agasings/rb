c398a40db625b8063d78922ccd2f9b07d74181e9
Gitaek kwon <break@kimsq.com>
2019-06-25 10:37:12 +0900

    ckeditor5 셋팅시 로더 추가

--- plugins/ckeditor5/import.desktop.php
+++ plugins/ckeditor5/import.desktop.php
@@ -10,6 +10,11 @@ if(!defined('__KIMS__')) exit;
 </style>
 
 <div class="rb-article">
+	<div data-role="loader">
+		<div class="d-flex justify-content-center align-items-center border text-muted bg-white" style="height:215px">
+			<div class="spinner-border mr-2" role="status"></div>
+		</div>
+	</div>
 	<textarea name="content" class="form-control ckeditor d-none" id="ckeditor_textarea"><?php echo $__SRC__?></textarea>
 </div>
 
@@ -30,6 +35,9 @@ $(function() {
 		.create( document.querySelector( '#ckeditor_textarea' ),{
 			language: 'ko',
 		} )
+		.then( editor => {
+			$('[data-role="loader"]').addClass('d-none') //로더 제거
+		})
 		.catch( error => {
 				console.error( error );
 		} );
