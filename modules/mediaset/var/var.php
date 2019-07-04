52168b959c359fad7c206e4390598e9516876283
Gitaek kwon <break@kimsq.com>
2019-07-04 09:06:34 +0900

    미디어셋 설정정보 초기화

--- modules/mediaset/var/var.php
+++ modules/mediaset/var/var.php
@@ -2,7 +2,7 @@
 $d['mediaset']['maxnum_file'] = "10";
 $d['mediaset']['maxsize_file'] = "31457280";
 $d['mediaset']['thumbsize'] = "1200";
-$d['mediaset']['use_fileserver'] = "2";
+$d['mediaset']['use_fileserver'] = "";
 $d['mediaset']['ftp_type'] = "";
 $d['mediaset']['ftp_host'] = "";
 $d['mediaset']['ftp_port'] = "21";
@@ -11,9 +11,8 @@ $d['mediaset']['ftp_pasv'] = "";
 $d['mediaset']['ftp_pass'] = "";
 $d['mediaset']['ftp_folder'] = "";
 $d['mediaset']['ftp_urlpath'] = "";
-
-$d['mediaset']['S3_KEY'] = "AKIAU753OV4564R53OFM";
-$d['mediaset']['S3_SEC'] = "XUu1f+JEv2iSnjc2mkQRPCte5e0uQPAPCaMmYXFd";
-$d['mediaset']['S3_REGION'] = "ap-northeast-2";
-$d['mediaset']['S3_BUCKET'] = "kimsqcom";
+$d['mediaset']['S3_KEY'] = "";
+$d['mediaset']['S3_SEC'] = "";
+$d['mediaset']['S3_REGION'] = "";
+$d['mediaset']['S3_BUCKET'] = "";
 ?>
