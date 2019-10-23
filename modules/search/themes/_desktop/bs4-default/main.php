f5d4dd7aa8d160c15786cc8956a3e3057c0a44d3
Gitaek kwon <break@kimsq.com>
2019-10-23 15:45:49 +0900

    Update main.php

--- modules/search/themes/_desktop/bs4-default/main.php
+++ modules/search/themes/_desktop/bs4-default/main.php
@@ -19,9 +19,7 @@
 
 					<div class="media align-items-center">
 						<h1 class="h2 mb-0 mr-4">
-							<a class="logo" href="<?php  echo RW(0) ?>">
-								<?php echo $d['layout']['header_file']?'<img src="'.$g['url_layout'].'/_var/'.$d['layout']['header_file'].'">':stripslashes($d['layout']['header_title'])?>
-							</a>
+							통합검색
 						</h1>
 						<div class="media-body">
 							<div class="input-group input-group-lg shadow-sm dropdown">
@@ -35,51 +33,6 @@
 				</div><!-- /.col-8 -->
 
 				<div class="col-4">
-
-					<ul class="nav flex-row-reverse">
-						<?php if ($my['uid']): ?>
-						<li class="nav-item dropdown">
-						  <a class="nav-link dropdown-toggle text-reset" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-role="tooltip" title="프로필보기 및 회원계정관리">
-								<?php echo $my['email'] ?>
-						  </a>
-						  <div class="dropdown-menu dropdown-menu-right">
-						    <h6 class="dropdown-header"><?php echo $my['nic'] ?> 님</h6>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="<?php echo RW('m=post&mod=write')?>">
-									새 포스트
-								</a>
-						    <div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="<?php echo RW('mod=dashboard')?>">
-									대시보드
-								</a>
-								<div class="dropdown-divider"></div>
-						    <a class="dropdown-item" href="<?php echo getProfileLink($my['uid'])?>">
-									프로필
-								</a>
-						    <div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="<?php echo RW('mod=settings')?>">
-									설정
-								</a>
-								<button class="dropdown-item" type="button" data-act="logout" role="button">
-									로그아웃
-								</button>
-								<?php if ($my['admin']): ?>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item text-danger" href="/?m=admin&pickmodule=site&panel=Y" target="_top">관리자모드</a>
-								<?php endif; ?>
-						  </div>
-						</li>
-						<?php else: ?>
-						<li class="nav-item">
-							<a class="nav-link text-reset" href="#modal-join" data-toggle="modal" data-backdrop="static">회원가입</a>
-						</li>
-						<li class="nav-item position-relative">
-							<a class="nav-link text-reset" href="#modal-login" data-toggle="modal" data-backdrop="static" title="모달형 로그인">
-								로그인
-							</a>
-						</li>
-						<?php endif; ?>
-			    </ul>
 
 
 				</div><!-- /.col-4 -->
