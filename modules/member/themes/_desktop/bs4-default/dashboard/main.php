616285c63cf0c0a72e8ecf49511928802a83c6a1
Gitaek kwon <break@kimsq.com>
2019-10-24 16:13:43 +0900

    Update main.php

--- modules/member/themes/_desktop/bs4-default/dashboard/main.php
+++ modules/member/themes/_desktop/bs4-default/dashboard/main.php
@@ -2,7 +2,18 @@
   <div class="row">
     <div class="col-6">
 
-      <section class="">
+      <section>
+        <?php include $g['dir_module_skin'].'_recentPost.php';?>
+      </section>
+
+      <section class="mt-4">
+        <?php include $g['dir_module_skin'].'_recentList.php';?>
+      </section>
+
+    </div>
+    <div class="col-6">
+
+      <section>
         <header class="d-flex justify-content-between align-items-center py-2">
           <strong>포스트 현황</strong>
         </header>
@@ -36,17 +47,6 @@
       </section>
 
       <section class="mt-3">
-        <?php include $g['dir_module_skin'].'_recentPost.php';?>
-      </section>
-
-      <section class="mt-4">
-        <?php include $g['dir_module_skin'].'_recentList.php';?>
-      </section>
-
-    </div>
-    <div class="col-6">
-
-      <section>
         <?php include $g['dir_module_skin'].'_chart.php';?>
       </section>
 
