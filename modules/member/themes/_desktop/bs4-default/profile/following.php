92de7802d8c4ad2777f02b7bbbddb4bf8229526d
Gitaek kwon <break@kimsq.com>
2019-11-03 10:46:31 +0900

    프로필 구독정보

--- modules/member/themes/_desktop/bs4-default/profile/following.php
+++ modules/member/themes/_desktop/bs4-default/profile/following.php
@@ -1,28 +1,16 @@
 <?php
-$type = $type ? $type : 'following';
 $sort	= 'uid';
 $orderby= 'desc';
 $recnum	= 15;
-if($type == 'follower')
-{
-	$bbsque	= 'by_mbruid='.$_MP['uid'];
-	$_fmemberuid = 'my_mbruid';
-}
-if($type == 'following')
-{
-	$bbsque	= 'my_mbruid='.$_MP['uid'];
-	$_fmemberuid = 'by_mbruid';
-}
-if($type == 'friend')
-{
-	$bbsque	= 'by_mbruid='.$_MP['uid'].' and rel=1';
-	$_fmemberuid = 'my_mbruid';
-}
-if ($where && $keyword) $bbsque .= getSearchSql($where,$keyword,$ikeyword,'or');
-$RCD = getDbArray($table['s_friend'],$bbsque,'*',$sort,$orderby,$recnum,$p);
-$NUM = getDbRows($table['s_friend'],$bbsque);
+
+$mbrque	= 'my_mbruid='.$_MP['uid'];
+
+if ($where && $keyword) $mbrque .= getSearchSql($where,$keyword,$ikeyword,'or');
+$RCD = getDbArray($table['s_friend'],$mbrque,'*',$sort,$orderby,$recnum,$p);
+$NUM = getDbRows($table['s_friend'],$mbrque);
 $TPG = getTotalPage($NUM,$recnum);
 ?>
+
 
 <div class="page-wrapper row">
 	<div class="col-3 page-nav">
@@ -34,75 +22,81 @@ $TPG = getTotalPage($NUM,$recnum);
 
 		<?php include $g['dir_module_skin'].'_nav.php';?>
 
+		<ul class="list-group list-group-flush border-top-0">
 
+			<?php $i=0;while($_R=db_fetch_array($RCD)):$i++?>
+			<?php $num_follower = getProfileInfo($_R['by_mbruid'],'num_follower');?>
+			<?php $_isFollowing = getDbRows($table['s_friend'],'my_mbruid='.$my['uid'].' and by_mbruid='.$_R['by_mbruid']); ?>
 
+			<li class="list-group-item d-flex justify-content-between align-items-center px-0" data-mbruid="<?php echo $_R['by_mbruid'] ?>">
 
-
-					<?php $i=0;while($_R=db_fetch_array($RCD)):$i++?>
-					<?php $M = getUidData($table['s_mbrid'],$_R[$_fmemberuid])?>
-					<?php $_M=getDbData($table['s_mbrdata'],'memberuid='.$_R[$_fmemberuid],'*')?>
-					<?php $_add = explode('<split>',$_M['addfield'])?>
-
-					<div class="d-flex row w-100 py-4 border-bottom border-gray-light">
-						<div class="col-1 v-align-top">
-							<a href="/@<?php echo $M['id']?>"><img alt="@<?php echo $M['id']?>" class="avatar" height="50" src="<?php echo $g['url_root']?>/_var/avatar/<?php if($_M['photo']):?><?php echo $_M['photo']?><?php else:?>0.gif<?php endif?>" width="50"></a>
-						</div>
-						<div class="col-9 v-align-top pr-3">
-					    <a href="/@<?php echo $M['id']?>" class="d-inline-block no-underline mb-1">
-					      <span class="f4 link-gray-dark"><?php echo $_M['nic']?></span>
-					      <span class="link-gray pl-1"><?php echo $M['id']?></span>
-					    </a>
-
-					      <?php if ($_add[0]): ?><p class="mb-1 text-gray text-small"><?php echo $_add[0]?></p><?php endif; ?>
-					      <p class="text-gray text-small mb-0">
-					        <svg aria-hidden="true" class="octicon octicon-location" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
-									<?php echo $_M['location']?>
-									(<?php echo $g['grade']['m'.$_M['level']]?> <?php echo $_M['level']?>)
-					      </p>
-					  </div>
-						<div class="col-2 v-align-top text-right">
-							<button class="btn btn-light btn-sm" type="button" name="button">팔로우</button>
-
-							<?php if($my['uid']):?>
-							<?php if($my['uid']==$_M['memberuid']):?>
-							<a class="btnGray01 plusBlue filter">Follow</i></a>
-							<?php else:?>
-							<?php $ISF = getDbData($table['s_friend'],'my_mbruid='.$my['uid'].' and by_mbruid='.$_M['memberuid'],'uid')?>
-							<?php if($ISF['uid']):?>
-							<a href="<?php echo $g['s']?>/?r=<?php echo $r?>&amp;m=<?php echo $m?>&amp;a=friend_unfollow&amp;fuid=<?php echo $ISF['uid']?>&amp;mbruid=<?php echo $_M['memberuid']?>&amp;reload=Y" class="btnGray01 plusBlue hand" target="_action_" onclick="return confirm('정말로 Unfollow 하시겠습니까?');"><i><s>Unfollow</s></i></a>
-							<?php else:?>
-							<?php $ISF = getDbData($table['s_friend'],'my_mbruid='.$_M['memberuid'].' and by_mbruid='.$my['uid'],'uid')?>
-							<a href="<?php echo $g['s']?>/?r=<?php echo $r?>&amp;m=<?php echo $m?>&amp;a=friend_follow&amp;fuid=<?php echo $ISF['uid']?>&amp;mbruid=<?php echo $_M['memberuid']?>&amp;reload=Y" class="btnGray01 plusBlue hand" target="_action_" onclick="return confirm('정말로 Follow 하시겠습니까?');"><i><s>Follow</s></i></a>
-							<?php endif?>
-							<?php endif?>
-
-							<?php else:?>
-							<a class="btnGray01 plusBlue filter">Follow</a>
-							<?php endif?>
-
-					  </div>
+				<div class="media w-75 align-items-center">
+					<a href="<?php echo getProfileLink($_R['by_mbruid'])?>" class="mr-3">
+						<img alt="" class="rounded-lg border" src="<?php echo getAvatarSrc($_R['by_mbruid'],'50') ?>" width="50" height="50" >
+					</a>
+					<div class="media-body">
+						<a href="<?php echo getProfileLink($_R['by_mbruid'])?>" class="text-decoration-none text-reset">
+							<span class="f4 link-gray-dark"><?php echo getProfileInfo($_R['by_mbruid'],'nic')?></span>
+						</a>
+						<p class="text-muted small mb-0">
+							구독자 <span data-role="num_follower"><?php echo number_format($num_follower)?></span>명
+						</p>
 					</div>
+				</div><!-- /.media -->
 
-					<?php endwhile?>
-					<?php if(!db_num_rows($RCD)):?>
-						<div class="blankslate mt-4">
-						  <i class="fa fa-user-plus fa-2x text-gray-light" aria-hidden="true"></i>
-						  <h3 class="mt-2">아직 팔로잉하는 사용자가 없습니다.</h3>
-						  <p>킴스큐의 <a href="">소셜활동</a>에 대해 자세히 확인해 보십시오.</p>
-						</div>
-					<?php else:?>
-					<nav aria-label="Page navigation" class="mt-4">
-						<ul class="pagination justify-content-center">
-							<?php $_N =  './'.$mbrid.'?page=following&' ?>
-			        <?php echo getPageLink(10,$p,$TPG,$_N)?>
-						</ul>
-					</nav>
+				<div class="">
+
+					<?php if ($my['uid']): ?>
+					<?php if($my['uid']!=$_R['by_mbruid']):?>
+					<button type="button" class="btn btn-primary btn-sm<?php echo $_isFollowing ?' active':''?>"
+					 data-act="actionIframe"
+					 data-toggle="button"
+					 data-role="follow"
+					 data-url="<?php echo $g['s'].'/?r='.$r.'&amp;m=member&amp;a=profile_follow&amp;mbruid='.$_R['by_mbruid']?>">
+						팔로우
+					</button>
 					<?php endif?>
+					<?php else: ?>
+					<span class="dropdown">
+						<button type="button" class="btn btn-primary" data-toggle="dropdown">
+							팔로우
+						</button>
+						<div class="dropdown-menu dropdown-menu-right shadow" style="min-width: 350px;">
+							<div class="py-3 px-4">
+								<h6><?php echo $M1[$_HS['nametype']] ?>님의 포스트를 구독하시겠습니까?</h6>
+								<p class="f13 text-muted mb-0">구독하려면 로그인하세요.</p>
+							</div>
+							<div class="dropdown-divider"></div>
+							<div class="px-3 text-right">
+								<button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-target="#modal-login">
+									로그인
+								</button>
+							</div>
+						</div>
+					</span>
+					<?php endif; ?>
 
+			  </div>
 
+			</li><!-- /.list-group-item  -->
+			<?php endwhile?>
 
+		</ul>
 
+		<?php if(!$NUM):?>
 
+		<div class="d-flex align-items-center justify-content-center" style="height: 40vh">
+			<div class="text-muted">자료가 없습니다.</div>
+		</div>
+
+		<?php else:?>
+		<nav aria-label="Page navigation" class="mt-4">
+			<ul class="pagination justify-content-center">
+				<?php $_N =  './'.$mbrid.'?page=follower&' ?>
+        <?php echo getPageLink(10,$p,$TPG,$_N)?>
+			</ul>
+		</nav>
+		<?php endif?>
 
 	</div><!-- /.page-main -->
 </div><!-- /.page-wrapper -->
