9ac23346abfceb5119dd5eea40da3ae9e48fc1d1
Gitaek kwon <break@kimsq.com>
2020-01-28 16:03:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,114 +9,108 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-
     <style>
-    /*
-    * Globals
-    */
+      a,
+      a:focus,
+      a:hover {
+      color: #fff;
+      }
 
-    /* Links */
-    a,
-    a:focus,
-    a:hover {
-    color: #fff;
-    }
-
-    /* Custom default button */
-    .btn-secondary,
-    .btn-secondary:hover,
-    .btn-secondary:focus {
-    color: #333;
-    text-shadow: none; /* Prevent inheritance from `body` */
-    background-color: #fff;
-    border: .05rem solid #fff;
-    }
+      /* Custom default button */
+      .btn-secondary,
+      .btn-secondary:hover,
+      .btn-secondary:focus {
+      color: #333;
+      text-shadow: none; /* Prevent inheritance from `body` */
+      background-color: #fff;
+      border: .05rem solid #fff;
+      }
 
 
-    /*
-    * Base structure
-    */
+      /*
+      * Base structure
+      */
 
-    html,
-    body {
-    height: 100%;
-    background-color: #333;
-    }
+      html,
+      body {
+      height: 100%;
+      background-color: #333;
+      }
 
-    body {
-    display: -ms-flexbox;
-    display: flex;
-    color: #fff;
-    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
-    box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
-    }
+      body {
+      display: -ms-flexbox;
+      display: flex;
+      color: #fff;
+      text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+      box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+      }
 
-    .cover-container {
-    max-width: 42em;
-    }
-
-
-    /*
-    * Header
-    */
-    .masthead {
-    margin-bottom: 2rem;
-    }
-
-    .masthead-brand {
-    margin-bottom: 0;
-    }
-
-    .nav-masthead .nav-link {
-    padding: .25rem 0;
-    font-weight: 700;
-    color: rgba(255, 255, 255, .5);
-    background-color: transparent;
-    border-bottom: .25rem solid transparent;
-    }
-
-    .nav-masthead .nav-link:hover,
-    .nav-masthead .nav-link:focus {
-    border-bottom-color: rgba(255, 255, 255, .25);
-    }
-
-    .nav-masthead .nav-link + .nav-link {
-    margin-left: 1rem;
-    }
-
-    .nav-masthead .active {
-    color: #fff;
-    border-bottom-color: #fff;
-    }
-
-    @media (min-width: 48em) {
-    .masthead-brand {
-      float: left;
-    }
-    .nav-masthead {
-      float: right;
-    }
-    }
+      .cover-container {
+      max-width: 42em;
+      }
 
 
-    /*
-    * Cover
-    */
-    .cover {
-    padding: 0 1.5rem;
-    }
-    .cover .btn-lg {
-    padding: .75rem 1.25rem;
-    font-weight: 700;
-    }
+      /*
+      * Header
+      */
+      .masthead {
+      margin-bottom: 2rem;
+      }
+
+      .masthead-brand {
+      margin-bottom: 0;
+      }
+
+      .nav-masthead .nav-link {
+      padding: .25rem 0;
+      font-weight: 700;
+      color: rgba(255, 255, 255, .5);
+      background-color: transparent;
+      border-bottom: .25rem solid transparent;
+      }
+
+      .nav-masthead .nav-link:hover,
+      .nav-masthead .nav-link:focus {
+      border-bottom-color: rgba(255, 255, 255, .25);
+      }
+
+      .nav-masthead .nav-link + .nav-link {
+      margin-left: 1rem;
+      }
+
+      .nav-masthead .active {
+      color: #fff;
+      border-bottom-color: #fff;
+      }
+
+      @media (min-width: 48em) {
+      .masthead-brand {
+        float: left;
+      }
+      .nav-masthead {
+        float: right;
+      }
+      }
 
 
-    /*
-    * Footer
-    */
-    .mastfoot {
-    color: rgba(255, 255, 255, .5);
-    }
+      /*
+      * Cover
+      */
+      .cover {
+      padding: 0 1.5rem;
+      }
+      .cover .btn-lg {
+      padding: .75rem 1.25rem;
+      font-weight: 700;
+      }
+
+
+      /*
+      * Footer
+      */
+      .mastfoot {
+      color: rgba(255, 255, 255, .5);
+      }
     </style>
 
     <title>킴스큐 설치 - Rb2</title>
@@ -128,16 +122,21 @@
       </header>
 
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">킴스큐 Rb2 설치</h1>
-        <p class="lead">설명 입니다.</p>
+        <h1 class="cover-heading">무한한 가능성!<br>킴스큐Rb2 설치를 시작합니다.</h1>
+        <p class="lead">킴스큐Rb는 별도의 서버작업(패키지 다운로드,압축해제,퍼미션 조정등) 절차없이 쉽고 빠르게 설치를 진행할 것입니다.
+      		준비가 되셨으면 설치하기 버튼을 클릭해 주십시오.</p>
         <form>
           <div class="form-group">
-            <label for="">패키지 버전</label>
+            <label class="sr-only">패키지 버전</label>
             <select class="form-control custom-select custom-select-lg rounded-0">
-              <option value="">선택하세요.</option>
+              <option value="">설치버전을 선택하세요.</option>
+
+
+
+              
             </select>
           </div>
-          <button type="button" class="btn btn-primary rounded-0">다운로드</button>
+          <button type="button" class="btn btn-primary rounded-0">설치하기</button>
         </form>
       </main>
 
