780185b0f6e6bf4c6c9fe66faf98d95c467c2d3e
Gitaek kwon <break@kimsq.com>
2020-01-28 15:28:32 +0900

    Update index.php

---
+++ index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="//kimsq.com/_core/images/ico/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+    <style>
+    /*
+    * Globals
+    */
+
+    /* Links */
+    a,
+    a:focus,
+    a:hover {
+    color: #fff;
+    }
+
+    /* Custom default button */
+    .btn-secondary,
+    .btn-secondary:hover,
+    .btn-secondary:focus {
+    color: #333;
+    text-shadow: none; /* Prevent inheritance from `body` */
+    background-color: #fff;
+    border: .05rem solid #fff;
+    }
+
+
+    /*
+    * Base structure
+    */
+
+    html,
+    body {
+    height: 100%;
+    background-color: #333;
+    }
+
+    body {
+    display: -ms-flexbox;
+    display: flex;
+    color: #fff;
+    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+    box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+    }
+
+    .cover-container {
+    max-width: 42em;
+    }
+
+
+    /*
+    * Header
+    */
+    .masthead {
+    margin-bottom: 2rem;
+    }
+
+    .masthead-brand {
+    margin-bottom: 0;
+    }
+
+    .nav-masthead .nav-link {
+    padding: .25rem 0;
+    font-weight: 700;
+    color: rgba(255, 255, 255, .5);
+    background-color: transparent;
+    border-bottom: .25rem solid transparent;
+    }
+
+    .nav-masthead .nav-link:hover,
+    .nav-masthead .nav-link:focus {
+    border-bottom-color: rgba(255, 255, 255, .25);
+    }
+
+    .nav-masthead .nav-link + .nav-link {
+    margin-left: 1rem;
+    }
+
+    .nav-masthead .active {
+    color: #fff;
+    border-bottom-color: #fff;
+    }
+
+    @media (min-width: 48em) {
+    .masthead-brand {
+      float: left;
+    }
+    .nav-masthead {
+      float: right;
+    }
+    }
+
+
+    /*
+    * Cover
+    */
+    .cover {
+    padding: 0 1.5rem;
+    }
+    .cover .btn-lg {
+    padding: .75rem 1.25rem;
+    font-weight: 700;
+    }
+
+
+    /*
+    * Footer
+    */
+    .mastfoot {
+    color: rgba(255, 255, 255, .5);
+    }
+    </style>
+
+    <title>킴스큐 설치 - Rb2</title>
+  </head>
+  <body>
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">킴스큐 Rb2 설치</h1>
+        <p class="lead">설명 입니다.</p>
+        <form>
+          <div class="form-group">
+            <label for="">패키지 버전</label>
+            <select class="form-control custom-select custom-select-lg rounded-0">
+              <option value="">선택하세요.</option>
+            </select>
+          </div>
+          <button type="button" class="btn btn-primary rounded-0">다운로드</button>
+        </form>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner text-center">
+          <p>Copyright Redblock inc.</p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
