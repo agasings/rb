4290692b0e432ece6f409c56e8fb92bf582e27b3
Gitaek kwon <break@kimsq.com>
2020-01-30 16:13:08 +0900

    Update a.install.php

--- _install/a.install.php
+++ _install/a.install.php
@@ -53,6 +53,9 @@ DirMake($g['path_var'].'sitephp');
 DirMake($g['path_layout'].'default/_images');
 DirMake($g['path_switch'].'top');
 
+@chmod($g['path_var'],0707);
+@chmod($g['path_file'],0707);
+
 $kfile = $g['path_var'].'project.key.txt';
 $fp = fopen($kfile,'w');
 fwrite($fp,$key);
