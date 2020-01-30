a4912bb337babd98efa280c08194beb0f4bbfd51
Gitaek kwon <break@kimsq.com>
2020-01-30 11:07:59 +0900

    Update list.js

--- modules/bbs/themes/_mobile/rc-default/_js/list.js
+++ modules/bbs/themes/_mobile/rc-default/_js/list.js
@@ -9,8 +9,8 @@
 
 function getBbsList(bid,cat,search,page){
   var markup = localStorage.getItem('bbs-'+bid+'-listMarkup');
-  var markup_list=markup+'-list'; // 목록 마크업
-  var markup_item=markup+'-item'; // 아이템 마크업
+  var markup_list=markup?markup:'media'+'-list'; // 목록 마크업
+  var markup_item=markup?markup:'media'+'-item'; // 아이템 마크업
   var page = $(page);
   var container = page.find('[data-role="bbs-list"]');
 
