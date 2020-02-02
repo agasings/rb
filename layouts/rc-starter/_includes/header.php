3e37b96691a1a2f7f4dcaa0e4f30609553af28a0
Gitaek kwon <break@kimsq.com>
2020-02-02 11:30:45 +0900

    모바일 기본 레이아웃 개선

--- layouts/rc-starter/_includes/header.php
+++ layouts/rc-starter/_includes/header.php
@@ -1,7 +1,9 @@
 <header class="bar bar-nav bar-light bg-white px-0" data-snap-ignore="true">
   <a href="#drawer-left" data-toggle="drawer" class="icon icon-bars pull-left p-x-1" role="button"></a>
-  <a class="icon material-icons pull-right p-r-1 pl-1" role="button" data-toggle="drawer" href="#drawer-right" data-direction="right" data-showType="expand" data-history="true">
-    notifications_none <span class="badge badge-pill badge-danger noti-status" data-role="noti-status"><?php echo $my['num_notice']==0?'':$my['num_notice']?></span>
+
+  <a class="icon pull-right p-r-1 pl-1" role="button" data-toggle="drawer" href="#drawer-right" data-direction="right" data-showType="expand" data-history="true">
+    <span class="material-icons ">notifications_none</span>
+    <span class="badge badge-pill badge-danger noti-status" data-role="noti-status"><?php echo $my['num_notice']==0?'':$my['num_notice']?></span>
   </a>
 
   <?php if($d['layout']['header_search']=='true'):?>
