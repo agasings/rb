dc1d22a504e075fc8f9e9d409d2826bfb2eaefb3
Gitaek kwon <break@kimsq.com>
2020-02-03 10:19:43 +0900

    Update _import.head.php

--- layouts/rc-starter/_includes/_import.head.php
+++ layouts/rc-starter/_includes/_import.head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="theme-color" content="#333">
 
